566111d21ef13cc95954d8b719ac73c510c506cf
jus1d <artfa63@gmail.com>
2023-04-09 19:19:13 +0400

    add faucet.js (discord)

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,98 @@
-import express from 'express';
+import { Ed25519Keypair, JsonRpcProvider, RawSigner, TransactionBlock, TransactionArgument, SUI_SYSTEM_STATE_OBJECT_ID, Connection } from '@mysten/sui.js';
+import { SuiKit } from '@scallop-dao/sui-kit';
+import consoleStamp from 'console-stamp';
+import axios from 'axios';
+import fs from 'fs';
+import * as bip39 from 'bip39';
+import BigNumber from "bignumber.js";
+import HttpsProxyAgent from 'https-proxy-agent';
 
-const app = express();
-const port = 3000;
+const network: string = 'testnet';
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+consoleStamp(console, { format: ':date(HH:MM:ss)' });
+const parseFile = (fileName: string) => fs.readFileSync(fileName, "utf8").split('\n').map(str => str.trim()).filter(str => str.length > 10);
+// const provider = new JsonRpcProvider('https://fullnode.testnet.sui.io');
+const faucetUrl = `https://faucet.${network}.sui.io/gas`;
+const timeout = (s: number) => new Promise(res => setTimeout(res, s*1000));
+const toSui = (n: number) => n / 1000000000;
+const toSuiObjects = (n: number) => n * 1000000000;
+const secretKey = '0xc458ea8e447bfb1e2e6c52f39078cffa48154723cfaa012200055b97693cd437';
+const mnemonic: string = parseFile('mnemonic.txt')[0];
+const suiKit = new SuiKit({ mnemonics: mnemonic, networkType: 'testnet' });
 
-app.get('/faucet',  async (req, res) => {
-    
-    console.log(req.body.address)
 
-    const response = await fetch('https://faucet.devnet.sui.io/gas', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            FixedAmountRequest: {
-                recipient: req.body.address,
-            },
-        })
-    })
+const faucet = async (address: string) => {
+    try {
+        const response = await fetch(faucetUrl, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                FixedAmountRequest: {
+                    recipient: address,
+                }
+            })
+        });
+        
+        if (response.status === 200 || response.status === 201 || response.status === 202) {
+            const balance = await getBalance();
+            console.log(`Success. Address: ${address}. Balance: ${balance}`);
+        } else {
+            console.log(`Reveived response: ${response.status} ${response.statusText}`)
+        }
 
-    res.status(response.status).json({
-        "address": req.body.address
-    })
-});
+    } catch (error) {
+        console.log(`Error: ${error} Address: ${address}`)
+    }
+}
 
-app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
-});
+
+const stake = async (validatorAddress: string, amountSui: number) => {
+    console.log(`Sending ${amountSui} SUI to staking. Validator: ${validatorAddress}`);
+
+    await suiKit.stakeSui(toSuiObjects(amountSui), validatorAddress).then(() => {
+        console.log(`Stake SUI success`);
+    }).catch(error => {
+        console.log(error.message)
+    });
+}
+
+
+const startTokenFarming = async (delaySecs: number) => {
+
+    const wallets = parseFile('wallets.txt');
+    // const proxyList = parseFile('proxy.txt');
+
+    while (true) {
+        for (let i = 0; i < wallets.length; i++) {
+            await faucet(wallets[i]);
+            await timeout(delaySecs);
+        }
+    }
+}
+
+
+const getBalance = async (): Promise<number> => {
+    const objectsBalance = (await suiKit.getBalance('0x2::sui::SUI')).totalBalance;
+    const suiBalance = toSui(objectsBalance);
+
+    // console.log(`Balance: ${suiBalance} SUI`);
+
+    return suiBalance;
+}
+
+
+(async () => {
+
+    // const validators: Array<string> = parseFile('devnetValidators.txt');
+
+    startTokenFarming(60);
+
+    // while (true) {
+    //     for (let i = 0; i < validators.length; i++) {
+    //         await stake(validators[i], 0.1);
+    //         await getBalance();
+    //         await timeout(60);
+    //     }      
+    // }
+
+})()
---
+++ src/faucet.js
@@ -0,0 +1,44 @@
+import { Client } from 'discord.js-selfbot-v13';
+import consoleStamp from 'console-stamp';
+import { Random } from "random-js";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const random = new Random();
+const client = new Client({ checkUpdate: false });
+const timeout = seconds => new Promise(res => setTimeout(res, 1000 * seconds));
+consoleStamp(console, { format: '(->).yellow :date( HH:MM:ss ).blue.underline' });
+
+const faucetChannelId = '1037811694564560966';
+const suiAddress = '0xc4ba490f7c68cb4384fb672d31337d533bbd55afc52936f833086e3dc1fd13a4';
+const shortSuiAddress = `${suiAddress.slice(0, 6)}...${suiAddress.slice(suiAddress.length - 6, suiAddress.length)}`;
+
+
+const getRandomDelay = async () => {
+  return random.integer(121 * 60, 140 * 60);
+}
+
+
+(async () => {
+  
+  client.on('ready', async () => {
+    console.log(`logged in as ${client.user.username}#${client.user.discriminator}`);
+  
+    while (true) {
+      // client.channels.cache.get(faucetChannelId).send(`!faucet ${suiAddress}`);
+
+      console.log(`faucet initiated to ${shortSuiAddress}`);
+
+      let delay = await getRandomDelay();
+
+      console.log(`waiting \x1b[33m${delay}\x1b[0m seconds`);
+  
+      await timeout(delay);
+    }
+  });
+
+
+  
+  client.login(process.env.TOKEN);
+
+})()
