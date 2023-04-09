be7d0a6a4a410d1ca4b17bdd86f884fffe1b536c
jus1d <artfa63@gmail.com>
2023-04-09 19:27:20 +0400

    remove clear blanks

--- src/faucet.js
+++ src/faucet.js
@@ -13,11 +13,9 @@ const faucetChannelId = '1037811694564560966';
 const suiAddress = '0xc4ba490f7c68cb4384fb672d31337d533bbd55afc52936f833086e3dc1fd13a4';
 const shortSuiAddress = `${suiAddress.slice(0, 6)}...${suiAddress.slice(suiAddress.length - 6, suiAddress.length)}`;
 
-
 const getRandomDelay = async () => {
   return random.integer(121 * 60, 140 * 60);
 }
-
 
 (async () => {
   
@@ -26,19 +24,14 @@ const getRandomDelay = async () => {
   
     while (true) {
       client.channels.cache.get(faucetChannelId).send(`!faucet ${suiAddress}`);
-
       console.log(`faucet initiated to ${shortSuiAddress}`);
 
       let delay = await getRandomDelay();
-
       console.log(`waiting \x1b[33m${delay}\x1b[0m seconds`);
-  
       await timeout(delay);
     }
   });
 
-
-  
   client.login(process.env.TOKEN);
 
 })()
