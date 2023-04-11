6807da3363b38de617f79948aad098f8a4c24444
jus1d <artfa63@gmail.com>
2023-04-11 23:10:30 +0400

    remove getDate

--- src/faucet.js
+++ src/faucet.js
@@ -49,19 +49,6 @@ const parseDelay = async (delay) => {
   return delayStr;
 }
 
-const getDate = () => {
-  var now = new Date();
-
-  var hours = now.getHours();
-  var minutes = now.getMinutes();
-  var seconds = now.getSeconds();
-
-  hours = (hours < 10 ? "0" : "") + hours;
-  minutes = (minutes < 10 ? "0" : "") + minutes;
-  seconds = (seconds < 10 ? "0" : "") + seconds;
-
-  return hours + ":" + minutes + ":" + seconds;
-}
 
 (async () => {
   
